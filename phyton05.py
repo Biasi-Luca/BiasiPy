7017b85690f88ac10acd2b19983b21b395306687
Biasi-Luca <74982114+Biasi-Luca@users.noreply.github.com>
2020-12-01 22:04:11 +0100

    Update phyton05.py

--- phyton05.py
+++ phyton05.py
@@ -1,6 +1,6 @@
 
 
-stringa = input("Scrivi una stringa ")
+stringa = input("Scrivere una stringa ")
 lista_lettere = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 print("i numeri di volte in cui le lettere si ripetono all'interno della stringa è")
 stringa = list(stringa.upper())
