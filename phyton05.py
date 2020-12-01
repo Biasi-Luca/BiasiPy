0c8b4632b00046f94dbe1bf4434ff7a053ec5741
Biasi-Luca <74982114+Biasi-Luca@users.noreply.github.com>
2020-12-01 19:46:52 +0100

    numeri di lettere in una stringa

---
+++ phyton05.py
@@ -0,0 +1,11 @@
+
+
+stringa = input("Scrivi una stringa ")
+lista_lettere = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+print("i numeri di volte in cui le lettere si ripetono all'interno della stringa è")
+stringa = list(stringa.upper())
+for lettera in lista_lettere:
+    if stringa.count(lettera) > 0:
+      print(lettera, " = ",stringa.count(lettera))
+
+
