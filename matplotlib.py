0bc71110f4af9a066e7644a7a8aa5815b87d0c03
Biasi-Luca <74982114+Biasi-Luca@users.noreply.github.com>
2021-02-02 16:52:44 +0100

    Delete matplotlib.py

--- matplotlib.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-xpoints  = np.array([1,3,5,7,9,11,13])
-ypoints = np.array([20646,16376,19347,20709,23219,24110,21052])
-plt.plot(xpoints, ypoints)
-plt.show()
