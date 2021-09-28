d07800cb642de9733d58f5c6e1b08b64fd2c3eb5
Biasi-Luca <biasi.luca.s21@liceoviconapoli.it>
2021-09-28 13:28:44 +0200

    classi ed oggetti

--- oop/es1_classi_e_oggetti.py
+++ oop/es1_classi_e_oggetti.py
@@ -7,7 +7,7 @@ class film:
      self.regista = regista
      self.produzione = produzione  
    def scheda(self):
-        return f'\ntitolo "{self.titolo}"\nGenere: {self.genere}\nDurata:{self.durata}\nRegista:{self.regista}\nProduzione:{self.produzione}'
+        return f'\ntitolo {self.titolo}\nGenere: {self.genere}\nDurata:{self.durata}\nRegista:{self.regista}\nProduzione:{self.produzione}'
 
 bandersnatch= film("bandersnatch","fantascienza","90 minuti","David Slade","Netflix")
 
